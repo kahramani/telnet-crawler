38ce5ad4844af602cc4e541cd7b7211b37bc02a6
kahramani <polapierre@gmail.com>
2016-11-15 15:53:21 +0300

    initial commit

---
+++ src/main/java/com/kahramani/crawler/telnet/application/Application.java
@@ -0,0 +1,15 @@
+package com.kahramani.crawler.telnet.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by kahramani on 11/15/2016.
+ */
+@ComponentScan("com.kahramani.crawler.*")
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+}
