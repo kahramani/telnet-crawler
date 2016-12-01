4a176da1bd39651aeab7898c1472cb22c0c8e75b
kahramani <polapierre@gmail.com>
2016-12-01 07:38:39 +0300

    main class changed

--- src/main/java/com/kahramani/crawler/telnet/application/Application.java
+++ src/main/java/com/kahramani/crawler/telnet/application/Application.java
@@ -1,7 +1,11 @@
 package com.kahramani.crawler.telnet.application;
 
+import com.kahramani.crawler.telnet.service.OltTelnetService;
+import com.kahramani.crawler.telnet.service.SwitchTelnetService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,4 +16,17 @@ public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+        // run switch telnet service
+        SwitchTelnetService switchTelnetService = context.getBean(SwitchTelnetService.class);
+        switchTelnetService.start();
+
+        // run olt telnet service
+        OltTelnetService oltTelnetService = context.getBean(OltTelnetService.class);
+        oltTelnetService.start();
+
+        // destroy all singleton beans
+        ((AnnotationConfigApplicationContext) context).destroy();
+    }
 }
